a2a81d604ba8b44048efc4a904c692bb4be277d4
r.kugot <stream13k@gmail.com>
2017-06-07 20:30:26 +0300

    Add Rakefile

--- app.rb
+++ app.rb
@@ -2,7 +2,10 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:blog.db"
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "MyBlog"			
 end
